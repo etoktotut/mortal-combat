947ce279b7bcdb00bd43251ff1fb2b0e620028df
Dmitry Kevdin <80814064+etoktotut@users.noreply.github.com>
2021-03-19 14:49:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
     $('#app').vide({
-        mp4: "video/video.mp4",
-        webm: "video/video.webm",
-        ogv: "video/video.ogv",
+       // mp4: "video/video.mp4",
+       // webm: "video/video.webm",
+        //ogv: "video/video.ogv",
         
     });
 });
